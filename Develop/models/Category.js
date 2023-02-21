4250fd581f9d17952a4e3c904da4b883ec764596
Abewd <116496332+abewd@users.noreply.github.com>
2023-02-22 07:34:43 +1100

    added basic files, adding routes now

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, STRING } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
-
 Category.init(
   {
-    // define columns
+    id: {
+      // We want our ID to be an integer, cant be null, a primary key and allow auto increments
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // We want our category names to be strings and cant be null
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
